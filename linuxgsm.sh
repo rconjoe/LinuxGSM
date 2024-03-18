d309d5a7b7e62951a204774da9b7b72ff5a5f78b
trog <admin@trog.host>
2024-03-17 17:44:31 -0700

    source this branch

--- linuxgsm.sh
+++ linuxgsm.sh
@@ -52,10 +52,9 @@ userinput2="${2}"
 ## GitHub Branch Select
 # Allows for the use of different function files
 # from a different repo and/or branch.
-[ -n "${LGSM_GITHUBUSER}" ] && githubuser="${LGSM_GITHUBUSER}" || githubuser="GameServerManagers"
-[ -n "${LGSM_GITHUBREPO}" ] && githubrepo="${LGSM_GITHUBREPO}" || githubrepo="LinuxGSM"
-[ -n "${LGSM_GITHUBBRANCH}" ] && githubbranch="${LGSM_GITHUBBRANCH}" || githubbranch="master"
-
+githubuser="rconjoe"
+githubrepo="LinuxGSM"
+githubbranch="mods/metamod2"
 # Check that curl is installed before doing anything
 if [ ! "$(command -v curl 2> /dev/null)" ]; then
 	echo -e "[ FAIL ] Curl is not installed"
